7793b666142810035d7ca9fd54b2384cf49fb2e2
dral0w <99414694+dral0w@users.noreply.github.com>
2023-11-15 16:01:17 +0300

    4

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -4,11 +4,11 @@ public class Employee {
     public String name;
     public String position;
     public String email;
-    public long phoneNumber;
+    public String phoneNumber;
     public int salary;
     public int age;
 
-    public Employee(String name, String position, String email, long phoneNumber, int salary, int age) {
+    public Employee(String name, String position, String email, String phoneNumber, int salary, int age) {
         this.name = name;
         this.position = position;
         this.email = email;
