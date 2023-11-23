394dba42c5f570fda510471401812f2817218335
dral0w <99414694+dral0w@users.noreply.github.com>
2023-11-23 17:45:56 +0300

    4

--- src/com/company/Park.java
+++ src/com/company/Park.java
@@ -1,11 +1,36 @@
 package com.company;
 
-public class Park {
-    public String name;
+import java.util.ArrayList;
+import java.util.List;
 
-    public class Attraction {
-        public String name;
-        public int workingHours;
-        public int price;
+public class Park {
+    private List<Attraction> attractions;
+
+    public Park() {
+        attractions = new ArrayList<>();
+    }
+
+    public void addAttraction(String name, String time, double price) {
+        Attraction attraction = new Attraction(name, time, price);
+        attractions.add(attraction);
+    }
+
+    public void displayAttractions() {
+        for (Attraction attraction : attractions) {
+            System.out.println(attraction);
+        }
+    }
+
+    private class Attraction {
+
+        private String name;
+        private String time;
+        private double price;
+
+        public Attraction(String name, String time, double price) {
+            this.name = name;
+            this.time = time;
+            this.price = price;
+        }
     }
 }
