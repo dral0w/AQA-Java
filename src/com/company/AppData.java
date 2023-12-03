24b1123004be8082502d1a4169a81dec0d5e6a45
dral0w <99414694+dral0w@users.noreply.github.com>
2023-12-03 21:46:52 +0300

    6

--- src/com/company/AppData.java
+++ src/com/company/AppData.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.io.*;
+import java.util.Arrays;
+
 /*
 Для хранения данных использовать класс вида:
 public class AppData {
@@ -8,7 +11,7 @@ public class AppData {
    // ...
 }
  */
-public class AppData {
+public class AppData implements Serializable {
     private String[] header;
     private int[][] data;
 
@@ -20,20 +23,71 @@ public class AppData {
         this.header = header;
     }
 
-    public String output() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String s : header) {
-            stringBuilder.append(s);
-            stringBuilder.append("; ");
-        }
-        stringBuilder.append("\n");
-        for (int[] datum : data) {
-            for (int i : datum) {
-                stringBuilder.append(i);
-                stringBuilder.append("; ");
+    private String[] getHeader() {
+        return header;
+    }
+
+    //Сохранение данных в файл
+    public void write() {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("file.csv"))){
+            bufferedWriter.write(String.join(";", getHeader()));
+            bufferedWriter.newLine();
+
+            for (int[] datum : data) {
+                bufferedWriter.write(String.join(";", intArrayToStringArray(datum)));
+                bufferedWriter.newLine();
             }
-            stringBuilder.append("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return stringBuilder.toString();
+    }
+
+    //Загрузка данных из файла
+    public void read() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("file.csv"))){
+            String header = bufferedReader.readLine();
+            setHeader(header.split(";"));
+
+            String line;
+            int rowCount = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] values = line.split(";");
+                int[] row = stringArrayToIntArray(values);
+                addDataRow(rowCount, row);
+                rowCount++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static int[] stringArrayToIntArray(String[] array) {
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = Integer.parseInt(array[i]);
+        }
+        return result;
+    }
+
+    private static String[] intArrayToStringArray(int[] array) {
+        String[] result = new String[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = String.valueOf(array[i]);
+        }
+        return result;
+    }
+
+    private void addDataRow(int rowIndex, int[] rowData) {
+        if (rowIndex >= 0 && rowIndex < data.length) {
+            data[rowIndex] = rowData;
+        }
+    }
+
+    public void getInformation() {
+        System.out.println(Arrays.toString(header));
+        for (int[] datum : data) {
+            System.out.println(Arrays.toString(datum));
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,14 @@
 package com.company;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        //Реализовать сохранение данных в csv файл;
-        try {
-            FileOutputStream file = new FileOutputStream("D://File/file.csv");
-            AppData data = new AppData();
-            data.setHeader(new String[]{"Value 1", "Value 2", "Value 3"});
-            data.setData(new int[][]{new int[]{100, 200, 123}, new int[]{300, 400, 500}});
-            file.write(data.output().getBytes());
-            file.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //Реализовать загрузку данных из csv файла. Файл читается целиком.
-        try {
-            FileInputStream fileInputStream = new FileInputStream("D://File/file.csv");
-            byte[] bytes = fileInputStream.readAllBytes();
-            fileInputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        AppData data = new AppData();
+        data.setHeader(new String[]{"Value 1", "Value 2", "Value 3"});
+        data.setData(new int[][]{new int[]{100, 200, 123}, new int[]{300, 400, 500}});
+        data.write();
+        data.getInformation();
+        data.read();
+        data.getInformation();
     }
 }
