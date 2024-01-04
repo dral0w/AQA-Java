79da42c02296392d30c08886b80ccab7b65c3c4e
dral0w <dralow221@gmail.com>
2024-01-04 16:28:34 +0300

    17

---
+++ src/test/java/Lesson17Tests.java
@@ -0,0 +1,87 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Lesson17Tests {
+
+    AndroidDriver<AndroidElement> driver = null;
+
+    @BeforeEach
+    public void setUp() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_3a:5554");
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+
+        try {
+            driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+        }
+
+       driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Calculator\"]")).click();
+    }
+
+    @Test
+    public void additionTest() {
+        driver.findElement(By.id("com.google.android.calculator:id/digit_2")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/op_add")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/digit_2")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/eq")).click();
+
+        assertEquals("4", driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText());
+    }
+
+    @Test
+    public void subtractionTest() {
+        driver.findElement(By.id("com.google.android.calculator:id/digit_2")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/op_sub")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/digit_2")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/eq")).click();
+
+        assertEquals("0", driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText());
+    }
+
+    @Test
+    public void multiplicationTest() {
+        driver.findElement(By.id("com.google.android.calculator:id/digit_2")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/op_mul")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/digit_3")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/eq")).click();
+
+        assertEquals("6", driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText());
+    }
+
+    @Test
+    public void divisionTest() {
+        driver.findElement(By.id("com.google.android.calculator:id/digit_8")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/op_div")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/digit_4")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/eq")).click();
+
+        assertEquals("2", driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.pressKey(new KeyEvent(AndroidKey.HOME));
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
